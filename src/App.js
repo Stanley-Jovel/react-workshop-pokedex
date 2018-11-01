264d8a2f92cbd5619c5592a66ecaaaf0013bcb94
Stanley Jovel <stanley@generalui.com>
2018-11-01 08:41:11 -0600

    step 5: use axios to fetch a single pokemon

--- src/App.js
+++ src/App.js
@@ -1,18 +1,30 @@
 import React, { Component } from 'react';
+import axios from 'axios'
 import './App.css';
 
 class App extends Component {
+  state = {
+    pokemon1: null
+  }
+
+  componentDidMount = () => {
+    axios.get('https://pokeapi.co/api/v2/pokemon/1/')
+      .then(response => {
+        this.setState({
+          pokemon1: response.data
+        })
+      })
+  }
+
   render() {
     return (
       <div className="App">
-        <div>
-          <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/1.png" alt=""/>
-          <label>bulbasaur</label>
-        </div>
-        <div>
-          <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/2.png" alt=""/>
-          <label>ivysaur</label>
-        </div>
+        { this.state.pokemon1 &&
+          <div>
+            <img src={this.state.pokemon1.sprites.front_default} alt=""/>
+            <label>{this.state.pokemon1.name}</label>
+          </div>
+        }
       </div>
     );
   }
