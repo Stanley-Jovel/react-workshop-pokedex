ef07d97988098b97e47ff6420f635ab9d58c189a
Stanley Jovel <stanley@generalui.com>
2018-11-01 09:19:31 -0600

    step 7: load pokemons by mapping through an array

--- src/App.js
+++ src/App.js
@@ -1,17 +1,28 @@
 import React, { Component } from 'react';
-import axios from 'axios'
 import './App.css';
 import Pokemon from './Pokemon'
+import Axios from 'axios';
 
 class App extends Component {
+  state = {
+    pokemons: []
+  }
+
+  componentDidMount = async () => {
+    const response = await Axios.get('https://pokeapi.co/api/v2/pokemon/')
+
+    this.setState({
+      pokemons: response.data.results.slice(0, 50)
+    })
+  }
   render() {
     return (
       <div className="App">
-        <Pokemon url='https://pokeapi.co/api/v2/pokemon/1/'/>
-        <Pokemon url='https://pokeapi.co/api/v2/pokemon/2/'/>
-        <Pokemon url='https://pokeapi.co/api/v2/pokemon/3/'/>
-        <Pokemon url='https://pokeapi.co/api/v2/pokemon/4/'/>
-        <Pokemon url='https://pokeapi.co/api/v2/pokemon/5/'/>
+        {
+          this.state.pokemons.map(pokemon => (
+            <Pokemon key={pokemon.name} url={pokemon.url}/>
+          ))
+        }
       </div>
     );
   }
